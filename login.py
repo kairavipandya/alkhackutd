a2fd4edd452d4ad6bc507c97b48639e9203ec762
kairavipandya <104184341+kairavipandya@users.noreply.github.com>
2023-11-05 11:16:34 -0600

    added login. py and agent.html

--- login.py
+++ login.py
@@ -1,31 +1,97 @@
 import streamlit as st
 
-# Add a title and header
-st.title('State Farm')
+# Custom CSS to inject into the Streamlit page
+st.markdown(
+    """
+    <style>
+    .login-box {
+        background-color: #ffffff;
+        padding: 10px;
+        border-radius: 10px;
+        box-shadow: 5px 5px 5px #aaaaaa;
+    }
+    .login-button {
+        background-color: #e60000;
+        color: white;
+        padding: 10px 24px;
+        margin: 8px 0;
+        border: none;
+        cursor: pointer;
+        width: 100%;
+        opacity: 0.9;
+        border-radius: 10px;
+    }
+    .login-button:hover {
+        opacity: 1;
+    }
+    .stTextInput>div>div>input {
+        color: #000;
+        background-color: #FFF;
+        border-radius: 5px;
+        border: 1px solid #ced4da;
+    }
+    .stButton>button {
+        width: 100%;
+        border-radius: 10px;
+        background-color: #e60000;
+        color: white;
+        padding: 10px 24px;
+        margin: 8px 0;
+        border: none;
+    }
+    .stButton>button:hover {
+        background-color: #cc0000;
+    }
+    .agent-header, .customer-header {
+        color: #CE0606;
+        text-align: center;
+        font-family: 'Inter', sans-serif;
+        font-size: 60px;
+        font-weight: 600;
+        line-height: normal;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
 
-# Create two columns for the Agent and Customer login forms
-col1, col2 = st.columns(2)
+# Create the layout with columns
+col1, col2 = st.columns([1, 1])
 
 with col1:
-    st.header("Agent")
+    # Create a box for the Agent login form
+    st.markdown('<div class="login-box">', unsafe_allow_html=True)
+    st.markdown('<h2 class="agent-header">Agent</h2>', unsafe_allow_html=True)
     agent_username = st.text_input("Username", key="agent_username")
     agent_password = st.text_input(
         "Password", type="password", key="agent_password")
-    agent_login = st.button("Log In", key="agent_login")
-
-    # Here you would handle the login logic for the agent
-    if agent_login:
-        st.write("Login logic for Agent")
+    if st.button('Log In', key='agent_login'):
+        # Perform login action
+        st.write("Agent Login Clicked")  # Placeholder for actual login logic
+    st.markdown('</div>', unsafe_allow_html=True)
 
 with col2:
-    st.header("Customer")
+    # Create a box for the Customer login form
+    st.markdown('<div class="login-box">', unsafe_allow_html=True)
+    st.markdown('<h2 class="customer-header">Customer</h2>',
+                unsafe_allow_html=True)
     customer_username = st.text_input("Username", key="customer_username")
     customer_password = st.text_input(
         "Password", type="password", key="customer_password")
-    customer_login = st.button("Log In", key="customer_login")
+    if st.button('Log In', key='customer_login'):
+        # Perform login action
+        # Placeholder for actual login logic
+        st.write("Customer Login Clicked")
+    st.markdown('</div>', unsafe_allow_html=True)
 
-    # Here you would handle the login logic for the customer
-    if customer_login:
-        st.write("Login logic for Customer")
-
-# You could add additional functionality like handling logins, displaying messages, etc.
+# Set background color
+st.markdown(
+    """
+    <style>
+    .stApp {
+        background: #8c0000;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
